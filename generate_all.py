a1d35668f7c80649d0f658a0bb8bab7b51098fa6
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2025-05-27 15:59:57 +0200

    generate_all.py

---
+++ generate_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+git_dir = os.path.dirname( os.path.realpath(__file__) )
+join = os.path.join
+cwd = os.getcwd()
+
+parser = argparse.ArgumentParser( description = 'Generate full dataset', formatter_class=argparse.ArgumentDefaultsHelpFormatter )
+parser.add_argument( "input_directory", help="input directory" )
+parser.add_argument( "output_directory", help="output directory" )
+parser.add_argument( "-n", "--number_of_samples", help="number of SDF samples per model", type= int )
+parser.add_argument( "-p", "--processes", help="number of processes for SDF computation", default = os.cpu_count(), type = int )
+args = parser.parse_args()
+
+input_dir = os.path.realpath( args.input_directory )
+output_dir = os.path.realpath( args.output_directory )
+out_meshes_dir = join( output_dir, "all" )
+os.system( f"python {join( git_dir, "generate_data_dirs.py" )} {input_dir} {out_meshes_dir}" )
+
+os.chdir(output_dir)
+os.system( f"python {join( git_dir, "generate_splits.py" )} {out_meshes_dir}" )
+os.chdir(cwd)
+
+data_name = output_dir.split( "/" ).pop()
+test_split = join( output_dir, data_name + "_all_test.json" )
+train_split = join( output_dir, data_name + "_all_train.json" )
+
+sdf_args = [ "python", join( git_dir, "preprocess_data.py" ), "-d", output_dir, "-s", output_dir, "--preprocessMeshPath", join( git_dir, "generate_distance_data.py" ), "--threads", str( args.processes ) ]
+if args.number_of_samples : sdf_args.extend( [ "--sdf_samples", str( args.number_of_samples ) ] )
+sdf_args.extend( [ "--split", test_split ] )
+print( " ".join( sdf_args ) )
+os.system( " ".join( sdf_args ) )
+
+sdf_args[ -1 ] = train_split
+sdf_args.extend( [ "--test" ] )
+os.system( " ".join( sdf_args ) )
